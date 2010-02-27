3322686b54e3ab27a453d48349e985cda12038c4
James Ottaway <james@ottaway.mp>
2010-02-27 11:47:50 +1000

    Added 'to_slug' helper and updated 'site' helper to take advantage of it

--- helpers.rb
+++ helpers.rb
@@ -11,7 +11,16 @@ helpers do
     "<a href='#{address}'>#{text}</a>"
   end
   
-  def site(href, text, id)
+  def site(href, text, id = to_slug(text))
     "<a href='#{href}' id='#{id}'>#{text}</a>"
+  end
+  
+  def to_slug(input)
+    input = input.gsub(/[']+/, '')
+    input = input.gsub(/\W+/, ' ')
+    input = input.strip
+    input = input.downcase
+    input = input.gsub(' ', '-')
+    input
   end
 end
