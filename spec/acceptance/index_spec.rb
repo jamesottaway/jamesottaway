4ffbe397d1150b3f46887d063f81a2038c1eb276
James Ottaway <james@ottaway.mp>
2011-09-25 10:17:08 +1000

    Fixed syntax errors in acceptance spec

--- spec/acceptance/index_spec.rb
+++ spec/acceptance/index_spec.rb
@@ -6,20 +6,20 @@ describe 'index' do
   end
   
   it 'should have my name as the title' do
-    on :index { |page| page.title.should == 'James Ottaway' }
+    on(:index) { |page| page.title.should == 'James Ottaway' }
   end
   
   it 'should include my name in the header' do
-    on :index { |page| page.header.should include 'James Ottaway' }
+    on(:index) { |page| page.header.should include 'James Ottaway' }
   end
   
   [:bio, :online, :contact_me].each do |section|
     it "should have a #{section} div" do
-      on :index { |page| page.send("#{section}_div").should exist }
+      on(:index) { |page| page.send("#{section}_div").should exist }
     end
   end
   
   it 'should include my name in the footer' do
-    on :index { |page| page.footer.should include 'James Ottaway' }
+    on(:index) { |page| page.footer.should include 'James Ottaway' }
   end
 end
